fa02beb3562095573159f23ad1a3f753891ac671
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2020-11-28 22:49:12 +0100

    cleanup

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = require('./config');
 
 const PORT = app.get('port');
 
+const getPageTitle = title => `${title} | Georgi Nikolov`;
+
 app.listen(PORT, () => {
   process.stdout.write(`Point your browser to: http://localhost:${PORT}\n`);
 });
@@ -29,20 +31,12 @@ app.use((req, res, next) => {
   });
 });
 
-/*
- *  --[ INSERT YOUR ROUTES HERE ]--
- */
-
-/*
- * Route with documentation to build your project with prismic
- */
 app.get('/', (req, res) => {
   req.prismic.api.query(
     Prismic.Predicates.at('document.type', 'work'),
   ).then((response) => {
-    let projects = (response.results || []).reduce((acc, item, i) => {
+    const projects = (response.results || []).reduce((acc, item, i) => {
       const projectYear = parseInt(item.data.project_year[0].text);
-      
       if (!acc[projectYear]) {
         acc[projectYear] = [ item ];
       } else {
@@ -51,49 +45,21 @@ app.get('/', (req, res) => {
           item,
         ];
       }
-      
       return acc;
     }, {});
-    // console.log(projects)
 
-    res.render('body', { projects });
-  })
+    res.render('body', {
+      title: getPageTitle('Home'),
+      projects,
+    });
+  });
 });
 
 app.get('/project/:uid', (req, res) => {
   req.prismic.api.getByUID('work', req.params.uid).then(project => {
-    res.render('single', { project });
+    res.render('single', {
+      title: getPageTitle(project.data.project_title[0].text),
+      project,
+    });
   });
 })
-
-/*
- * Prismic documentation to build your project with prismic
- */
-app.get('/help', (req, res) => {
-  const repoRegexp = /^(https?:\/\/([-\w]+)\.[a-z]+\.(io|dev))\/api(\/v2)?$/;
-  const [_, repoURL, name, extension, apiVersion] = PrismicConfig.apiEndpoint.match(repoRegexp);
-  const { host } = req.headers;
-  const isConfigured = name !== 'your-repo-name';
-  res.render('help', {
-    isConfigured,
-    repoURL,
-    name,
-    host,
-  });
-});
-
-/*
- * Preconfigured prismic preview
- */
-app.get('/preview', (req, res) => {
-  const { token } = req.query;
-  if (token) {
-    req.prismic.api.previewSession(token, PrismicConfig.linkResolver, '/').then((url) => {
-      res.redirect(302, url);
-    }).catch((err) => {
-      res.status(500).send(`Error 500 in preview: ${err.message}`);
-    });
-  } else {
-    res.send(400, 'Missing token from querystring');
-  }
-});
