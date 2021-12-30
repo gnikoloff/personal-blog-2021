5de112caebef9c746951a59181d54d14be51a095
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2021-12-30 20:45:59 +0100

    formatting

--- app/index.js
+++ app/index.js
@@ -1,41 +1,41 @@
-import "normalize.css";
-import "./style.scss";
+import 'normalize.css'
+import './style.scss'
 
-import hljs from "highlight.js/lib/core";
+import hljs from 'highlight.js/lib/core'
 
-import xml from "highlight.js/lib/languages/xml";
-import javascript from "highlight.js/lib/languages/javascript";
-import glsl from "highlight.js/lib/languages/glsl";
+import xml from 'highlight.js/lib/languages/xml'
+import javascript from 'highlight.js/lib/languages/javascript'
+import glsl from 'highlight.js/lib/languages/glsl'
 
-import mediumZoom from "medium-zoom";
+import mediumZoom from 'medium-zoom'
 
-import "highlight.js/styles/codepen-embed.css";
+import 'highlight.js/styles/codepen-embed.css'
 
-hljs.registerLanguage("xml", xml);
-hljs.registerLanguage("javascript", javascript);
-hljs.registerLanguage("glsl", glsl);
+hljs.registerLanguage('xml', xml)
+hljs.registerLanguage('javascript', javascript)
+hljs.registerLanguage('glsl', glsl)
 
-const BACKEND_URL = location.origin;
+const BACKEND_URL = location.origin
 
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener('DOMContentLoaded', init)
 
 function init() {
-  if (document.body.classList.contains("homepage")) {
-    initHome();
+  if (document.body.classList.contains('homepage')) {
+    initHome()
   }
-  if (document.body.classList.contains("single-blog")) {
-    initSingleBlog();
+  if (document.body.classList.contains('single-blog')) {
+    initSingleBlog()
   }
-  if (document.body.classList.contains("single-work")) {
-    initSingleWork();
+  if (document.body.classList.contains('single-work')) {
+    initSingleWork()
   }
-  if (document.body.classList.contains("contact")) {
-    initContact();
+  if (document.body.classList.contains('contact')) {
+    initContact()
   }
 }
 
 function initSingleWork() {
-  const allImages = document.querySelectorAll(".single-work img");
+  const allImages = document.querySelectorAll('.single-work img')
   mediumZoom(allImages, {
     container: {
       width: innerWidth,
@@ -45,121 +45,121 @@ function initSingleWork() {
       right: 0,
       left: 0,
     },
-  });
+  })
 
-  document.querySelectorAll("pre code").forEach((block) => {
-    hljs.highlightBlock(block);
-  });
+  document.querySelectorAll('pre code').forEach((block) => {
+    hljs.highlightBlock(block)
+  })
 }
 
 function initSingleBlog() {
-  document.querySelectorAll("pre code").forEach((block) => {
-    hljs.highlightBlock(block);
-  });
+  document.querySelectorAll('pre code').forEach((block) => {
+    hljs.highlightBlock(block)
+  })
 }
 
 function initHome() {
-  const allWorks = document.getElementsByClassName("preview");
+  const allWorks = document.getElementsByClassName('preview')
   for (let i = 0; i < allWorks.length; i++) {
-    const workEl = allWorks[i];
-    const imageSrc = workEl.getAttribute("data-image");
-    const imageAlt = workEl.getAttribute("data-alt");
+    const workEl = allWorks[i]
+    const imageSrc = workEl.getAttribute('data-image')
+    const imageAlt = workEl.getAttribute('data-alt')
     const imageWrapper = workEl.getElementsByClassName(
-      "preview-image-wrapper"
-    )[0];
+      'preview-image-wrapper',
+    )[0]
     const makeImage = () => {
-      const img = document.createElement("img");
-      img.classList.add("project-preview-image");
-      img.setAttribute("src", imageSrc);
-      img.setAttribute("alt", imageAlt);
+      const img = document.createElement('img')
+      img.classList.add('project-preview-image')
+      img.setAttribute('src', imageSrc)
+      img.setAttribute('alt', imageAlt)
       requestAnimationFrame(() => {
-        img.classList.add("inView");
-      });
-      imageWrapper.appendChild(img);
-    };
+        img.classList.add('inView')
+      })
+      imageWrapper.appendChild(img)
+    }
     if (imageSrc) {
       if (supportIntersectObserver()) {
         const onObserve = (entries) => {
-          const entry = entries[0];
+          const entry = entries[0]
           if (entry.isIntersecting) {
-            makeImage();
-            observer.unobserve(workEl);
+            makeImage()
+            observer.unobserve(workEl)
           }
-        };
-        const observer = new IntersectionObserver(onObserve);
-        observer.observe(workEl);
+        }
+        const observer = new IntersectionObserver(onObserve)
+        observer.observe(workEl)
       } else {
-        makeImage();
+        makeImage()
       }
     }
   }
 }
 
 function initContact() {
-  const form = document.getElementById("contact-form");
-  const emailLoadbar = document.querySelector(".email-send-loader-wrapper");
+  const form = document.getElementById('contact-form')
+  const emailLoadbar = document.querySelector('.email-send-loader-wrapper')
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    const formParams = new URLSearchParams(formData).toString();
-    emailLoadbar.classList.add("visible");
-    const url = `${BACKEND_URL}/contact?${formParams}`;
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const formData = new FormData(form)
+    const formParams = new URLSearchParams(formData).toString()
+    emailLoadbar.classList.add('visible')
+    const url = `${BACKEND_URL}/contact?${formParams}`
     fetch(url, {
-      method: "POST",
+      method: 'POST',
     })
       .then((res) => res.json())
       .then((res) => {
-        emailLoadbar.classList.remove("visible");
-        const inputs = form.getElementsByTagName("input");
+        emailLoadbar.classList.remove('visible')
+        const inputs = form.getElementsByTagName('input')
         for (let i = 0; i < inputs.length; i++) {
-          if (inputs[i].getAttribute("type") !== "submit") {
-            inputs[i].value = "";
+          if (inputs[i].getAttribute('type') !== 'submit') {
+            inputs[i].value = ''
           }
         }
-        const textarea = form.getElementsByTagName("textarea");
+        const textarea = form.getElementsByTagName('textarea')
         for (let i = 0; i < textarea.length; i++) {
-          textarea[i].value = "";
+          textarea[i].value = ''
         }
 
-        if (res.type === "ERROR") {
-          console.error(JSON.parse(res.payload));
+        if (res.type === 'ERROR') {
+          console.error(JSON.parse(res.payload))
         } else {
           const formSuccess = document.getElementsByClassName(
-            "contact-form-success"
-          )[0];
+            'contact-form-success',
+          )[0]
           const formWrapper = document.getElementsByClassName(
-            "contact-form-wrapper"
-          )[0];
-          formSuccess.style.setProperty("display", "block");
-          formWrapper.style.setProperty("display", "none");
+            'contact-form-wrapper',
+          )[0]
+          formSuccess.style.setProperty('display', 'block')
+          formWrapper.style.setProperty('display', 'none')
           setTimeout(() => {
-            formSuccess.style.removeProperty("display");
-            formWrapper.style.removeProperty("display");
-          }, 8 * 1000);
+            formSuccess.style.removeProperty('display')
+            formWrapper.style.removeProperty('display')
+          }, 8 * 1000)
         }
       })
-      .catch(console.error);
-  });
-  const budgetInputWrapper = document.getElementById("budget-group");
-  const budgetInput = document.getElementById("budget");
-  const projectTypeSelect = document.getElementById("project-type");
+      .catch(console.error)
+  })
+  const budgetInputWrapper = document.getElementById('budget-group')
+  const budgetInput = document.getElementById('budget')
+  const projectTypeSelect = document.getElementById('project-type')
 
-  projectTypeSelect.addEventListener("change", (e) => {
-    if (e.target.value === "Project") {
-      budgetInput.setAttribute("required", "");
-      budgetInputWrapper.style.setProperty("display", "block");
+  projectTypeSelect.addEventListener('change', (e) => {
+    if (e.target.value === 'Project') {
+      budgetInput.setAttribute('required', '')
+      budgetInputWrapper.style.setProperty('display', 'block')
     } else {
-      budgetInput.removeAttribute("required");
-      budgetInputWrapper.style.removeProperty("display");
+      budgetInput.removeAttribute('required')
+      budgetInputWrapper.style.removeProperty('display')
     }
-  });
+  })
 }
 
 function supportIntersectObserver() {
   return (
-    "IntersectionObserver" in window &&
-    "IntersectionObserverEntry" in window &&
-    "intersectionRatio" in window.IntersectionObserverEntry.prototype
-  );
+    'IntersectionObserver' in window &&
+    'IntersectionObserverEntry' in window &&
+    'intersectionRatio' in window.IntersectionObserverEntry.prototype
+  )
 }
