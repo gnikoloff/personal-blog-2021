82a4eb940eacfaf9fea341d5374ff253ae14c7f1
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2022-01-28 14:08:04 +0100

    add cors

--- config.js
+++ config.js
@@ -1,31 +1,32 @@
-
 /**
  * Module dependencies.
  */
-const express = require('express');
-const favicon = require('serve-favicon');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-const errorHandler = require('errorhandler');
-const compression = require('compression');
-const path = require('path');
+const express = require('express')
+const favicon = require('serve-favicon')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const errorHandler = require('errorhandler')
+const compression = require('compression')
+const cors = require('cors')
+const path = require('path')
 
 module.exports = (() => {
-  const app = express();
+  const app = express()
 
   // all environments
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'pug');
+  app.set('port', process.env.PORT || 3001)
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'pug')
   // app.use(favicon('public/images/punch.png'));
   // app.use(logger('dev'));
-  app.use(compression());
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(methodOverride());
-  app.use(express.static(path.join(__dirname, 'dist')));
+  app.use(cors())
+  app.use(compression())
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(methodOverride())
+  app.use(express.static(path.join(__dirname, 'dist')))
 
-  app.use(errorHandler());
+  app.use(errorHandler())
 
-  return app;
-})();
+  return app
+})()
